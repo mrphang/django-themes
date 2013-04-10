42beb779d0ded61ef9492c936e8445f5385302e9
Michael Phang <phangm@trueaction.com>
2013-04-10 15:49:28 -0400

    Changed the UserTheme model to use settings.AUTH_USER_MODEL instead of auth.models.User.

--- themes/models.py
+++ themes/models.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from django.db import models
-from django.contrib.auth.models import User
+from django.conf import settings
 from django.utils.translation import ugettext_lazy as _, ugettext
 
 #class Theme(models.Model):
@@ -8,7 +8,7 @@ from django.utils.translation import ugettext_lazy as _, ugettext
 #    theme = models.IntegerField(_('Theme'), default=0)
 
 class UserTheme(models.Model):
-    user = models.ForeignKey(User, unique=True, related_name='user_theme')
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, unique=True, related_name='user_theme')
     theme = models.IntegerField(_("Theme"), default=0)
 
     def __unicode__(self):
